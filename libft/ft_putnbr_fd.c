90cd5a6b7310c2688873660de087fbd4ed4df943
Yuri Lee <ylee@c1r1s1.42seoul.kr>
2020-10-16 11:59:50 +0900

    modify putnbr_fd

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: ylee <ylee@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/15 15:41:15 by ylee              #+#    #+#             */
-/*   Updated: 2020/10/15 16:00:28 by ylee             ###   ########.fr       */
+/*   Updated: 2020/10/16 11:39:11 by ylee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,19 @@ void	ft_putnbr_fd(int n, int fd)
 
 	ft_bzero(num, 20);
 	if (n < 0)
-		ln = -n;
+		ln = (long long)n * (-1);
 	else
-		ln = n;
+		ln = (long long)n;
 	idx = 0;
 	while (ln > 0)
 	{
-		num[idx++] = ln % 10;
+		num[idx++] = ln % 10 + '0';
 		ln = ln / 10;
 	}
 	if (n < 0)
 		num[idx++] = '-';
 	if (n == 0)
 		num[idx++] = '0';
-	while (idx >= 0)
+	while (idx > 0)
 		write(fd, &num[--idx], 1);
 }
