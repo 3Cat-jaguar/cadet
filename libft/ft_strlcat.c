952ec043d06e5e6a231bd58cce219232a46bc2cc
Yuri Lee <ylee@c3r1s2.42seoul.kr>
2020-10-22 17:02:30 +0900

    modify strlcat

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,32 +6,33 @@
 /*   By: ylee <ylee@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/07 11:07:51 by ylee              #+#    #+#             */
-/*   Updated: 2020/10/13 13:03:38 by ylee             ###   ########.fr       */
+/*   Updated: 2020/10/22 17:02:12 by ylee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	i;
 	size_t	dst_len;
 	size_t	src_len;
 
-	dst_len = 0;
-	src_len = 0;
-	while (dst[dst_len])
-		dst_len++;
-	while (src[src_len])
-		src_len++;
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
 	if (dst_len > size)
 		return (src_len + size);
-	i = dst_len;
-	while (src[i - dst_len] && (i + 1) < size)
+	else if (dst_len == size)
+		return (dst_len + src_len);
+	while (*dst)
+		dst++;
+	while (dst && *src && (dst_len + 1) < size)
 	{
-		dst[i] = src[i - dst_len];
-		i++;
+		*dst = *src;
+		dst++;
+		src++;
+		dst_len++;
 	}
-	dst[i] = '\0';
+	if (dst && *dst != '\0')
+		*dst = '\0';
 	return (dst_len + src_len);
 }
--- test/Libftest/my_config.sh
+++ test/Libftest/my_config.sh
@@ -5,7 +5,7 @@
 ###################################
 
 #Edit the next line with the path of your libft project.
-PATH_LIBFT=../../../try_eval/libft/
+PATH_LIBFT=../../libft/
 
 #If you use a relative path, don't put double quotes.
 #For example :
