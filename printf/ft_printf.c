f6bbffb80430d6067bb6019f701445e07298d6e0
ylee <ylee@student.42seoul.kr>
2020-11-20 10:46:23 +0900

    ft_printf norm error check

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,12 +6,40 @@
 /*   By: ylee <ylee@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 10:43:39 by ylee              #+#    #+#             */
-/*   Updated: 2020/11/20 09:28:12 by ylee             ###   ########.fr       */
+/*   Updated: 2020/11/20 10:45:15 by ylee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
-#include <stdio.h>
+
+void	join_str_and_free(char **result, char *str)
+{
+	result[2] = ft_strjoin(result[0], str);
+	free(result[0]);
+	result[0] = NULL;
+	result[0] = ft_strdup(result[2]);
+	free(result[2]);
+	result[2] = NULL;
+}
+
+void	init_variables(int *idx, int *start, t_arg **arg, char **result)
+{
+	*idx = 0;
+	*start = 0;
+	*arg = NULL;
+	result[0] = ft_strdup("");
+}
+
+void	ending(char **result, int start, va_list ap)
+{
+	join_str_and_free(result, &result[3][start]);
+	write(1, result[0], ft_strlen(result[0]));
+	free(result[0]);
+	result[0] = NULL;
+	free(result[3]);
+	result[3] = NULL;
+	va_end(ap);
+}
 
 int		ft_printf(const char *str, ...)
 {
@@ -21,47 +49,23 @@ int		ft_printf(const char *str, ...)
 	t_arg	*arg;
 	char	*result[4];
 
-	idx = 0;
-	start = 0;
-	va_start(ap, str);
-	arg = NULL;
-	result[0] = ft_strdup("");
+	init_variables(&idx, &start, &arg, result);
 	result[3] = ft_strdup(str);
+	va_start(ap, str);
 	while (str[idx])
 	{
 		if (str[idx] == '%')
 		{
 			result[3][idx] = '\0';
-			result[2] = ft_strjoin(result[0], &result[3][start]);
-			free(result[0]);
-			result[0] = NULL;
-			result[0] = ft_strdup(result[2]);
-			free(result[2]);
-			result[2] = NULL;
+			join_str_and_free(result, &result[3][start]);
 			result[1] = find_percent(ap, arg, (char *)str, &idx);
 			start = idx + 1;
-			result[2] = ft_strjoin(result[0], result[1]);
-			free(result[0]);
-			result[0] = NULL;
-			result[0] = ft_strdup(result[2]);
-			free(result[2]);
-			result[2] = NULL;
+			join_str_and_free(result, result[1]);
 			free(result[1]);
 			result[1] = NULL;
 		}
 		idx++;
 	}
-	result[2] = ft_strjoin(result[0], &result[3][start]);
-	free(result[0]);
-	result[0] = NULL;
-	result[0] = ft_strdup(result[2]);
-	free(result[2]);
-	result[2] = NULL;
-	write(1, result[0], ft_strlen(result[0]));
-	free(result[0]);
-	result[0] = NULL;
-	free(result[3]);
-	result[3] = NULL;
-	va_end(ap);
+	ending(result, start, ap);
 	return (0);
 }
