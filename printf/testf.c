f4df04fc68aba7ed1c09402993d1c72fd471633d
ylee <ylee@student.42seoul.kr>
2021-01-08 16:58:22 +0900

    test type f (2)

--- printf/testf.c
+++ printf/testf.c
@@ -6,7 +6,7 @@
 /*   By: ylee <ylee@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 15:35:23 by ylee              #+#    #+#             */
-/*   Updated: 2021/01/07 14:46:44 by ylee             ###   ########.fr       */
+/*   Updated: 2021/01/08 16:58:09 by ylee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,44 @@ void	itobit(int size, unsigned f)
 	printf("\n");
 }
 
-int		power2(int n)
+long long	power_n(int n, int k)
 {
-	int		result;
+	long long	result;
 
 	result = 1;
-	while (n > 0)
+	while (k > 0)
 	{
-		result = result * 2;
-		n--;
+		result = result * n;
+		k--;
 	}
+	return (result);
+}
+
+char	*ft_uitoa_base2(unsigned int num)
+{
+	char			*base;
+	char			*result;
+	char			tmp[32];
+	unsigned int	uni;
+	int				idx;
+
+	base = "01";
+	uni = (unsigned int)num;
+	idx = 0;
+	while (uni != 0)
+	{
+		tmp[idx++] = base[(uni % 2)];
+		uni = uni / 2;
+	}
+	if (num == 0)
+		tmp[idx++] = '0';
+	tmp[idx] = '\0';
+	result = (char *)ft_calloc(idx, sizeof(char));
+	if (!result)
+		return (0);
+	while (--idx >= 0)
+		result[(int)uni++] = tmp[idx];
+	result[(int)uni] = '\0';
 	return (result);
 }
 
@@ -59,6 +87,10 @@ int		main(void)
 	int			jungsu;
 	char		*result;
 	int			sosu;
+	long long	real_sosu;
+	char		*sosu_string;
+	char		*sosu_string_base2;
+	int			sosu_base2_len;
 
 	realf = 123.123456789;
 	f = *(unsigned *)&realf;
@@ -79,10 +111,26 @@ int		main(void)
 	frac = 0;
 	jungsu = (f << 9) >> (size * 8 - exp);
 	printf("jungsu ? : %d\n", jungsu);
-	jungsu = jungsu + power2(exp);
+	jungsu = jungsu + (int)power_n(2, exp);
 	result = ft_strjoin(ft_itoa(jungsu), ".sosu");
 	printf("jungsu string : %s\n", result);
 	sosu = (f << (9 + exp)) >> (9 + exp);
-	itobit(size, sosu);
+	printf("sosu to int ? : %d\n", sosu);
+	sosu_string_base2 = ft_uitoa_base2((unsigned)sosu);
+	printf("sosu go string base 2 : %s\n", sosu_string_base2);
+	sosu_base2_len = (int)ft_strlen(sosu_string_base2);
+	printf("sosu string length : %d\n", sosu_base2_len);
+	real_sosu = 0;
+	while (i < sosu_base2_len)
+	{
+		printf("%lld\n", real_sosu);
+		real_sosu = (real_sosu * 10) + ((sosu_string_base2[i] - '0') * power_n(5, i + 1));
+		i++;
+	}
+	real_sosu = real_sosu * power_n(5, (size * 8 - 9 - exp) - sosu_base2_len);
+	printf("real sosu to int ? : %lld\n", real_sosu);
+	sosu_string = ft_lltoa(real_sosu);
+	printf("real sosu to string : %s\n", sosu_string);
+
 	return(0);
 }
